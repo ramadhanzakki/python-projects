797b2df888f0fbfb8136a2afcc5116661d0eb3b1
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2025-11-08 13:20:33 +0700

    feat: added recently played features with stacks

--- Music_playlist_manager/main.py
+++ Music_playlist_manager/main.py
@@ -1,24 +1,41 @@
 from src.SongNode import SongNode
 from src.Playlist import PlayList
 
-my_playlist = PlayList()
+print("========================================")
+print("    🚀 STARTING FEATURES TESTING 🚀    ")
+print("========================================")
 
+my_playlist = PlayList()
 my_playlist.add_song('Golden - HUNTRX/EJAE, Audrey Nuna, Rei Ami')
 my_playlist.add_song('In Another World - EJAE')
 my_playlist.add_song('FOCUS - Hearts2Hearts')
 my_playlist.add_song('House Party - VVUP')
 
+print("\n--- 🎵 Initial Playlist Created 🎵 ---")
 my_playlist.display()
 
-my_playlist.play_song('Golden - HUNTRX/EJAE, Audrey Nuna, Rei Ami')
-my_playlist.next_song()
-my_playlist.next_song()
-my_playlist.next_song()
-my_playlist.next_song()
-my_playlist.prev_song()
-my_playlist.play_song('Golden - HUNTRX/EJAE, Audrey Nuna, Rei Ami')
+print('\n--- Test 1: Test Empty History ---')
+my_playlist.show_history()
 
-print("\n--- 🔍 Intip isi Hash Table (song_index) ---")
-for title, node in my_playlist.song_index.items():
-    print(f"Key: '{title}'")
-    print(f"   -> Value: <SongNode object di memori, Title: {node.title}>")
+print('\n--- Test 2: Play Song ---')
+my_playlist.play_song('Golden - HUNTRX/EJAE, Audrey Nuna, Rei Ami')
+my_playlist.show_history()
+
+print('\n--- Test 3: Play next song ---')
+my_playlist.next_song()
+my_playlist.next_song()
+my_playlist.next_song()
+my_playlist.show_history()
+
+print('\n--- Test 4: Play Previous Song ---')
+my_playlist.prev_song()
+my_playlist.show_history()
+
+print('\n--- Test 5: Failed Action ---')
+my_playlist.play_song('House Party - VVUP')
+my_playlist.next_song()
+my_playlist.show_history()
+
+print("\n========================================")
+print("       🏁 TESTING IS FINNISED        🏁")
+print("========================================")
--- Music_playlist_manager/src/Playlist.py
+++ Music_playlist_manager/src/Playlist.py
@@ -6,6 +6,7 @@ class PlayList:
         self.tail = None
         self.current_song = None
         self.song_index = {}
+        self.play_history = []
 
 
     def add_song(self, title):
@@ -44,6 +45,7 @@ class PlayList:
             node_to_play = self.song_index[title]
             self.current_song = node_to_play
             print(f'▶️ Playing {self.current_song.title}')
+            self.play_history.append(self.current_song.title)
 
         else:
             print(f'[!] The song ‘{title}’ was not found in the playlist')
@@ -60,6 +62,7 @@ class PlayList:
         
         self.current_song = self.current_song.next
         print(f'▶️ Next up: {self.current_song.title}')
+        self.play_history.append(self.current_song.title)
 
     
     def prev_song(self):
@@ -73,3 +76,15 @@ class PlayList:
 
         self.current_song = self.current_song.prev
         print(f'▶️ Play previous: {self.current_song.title}')
+        self.play_history.append(self.current_song.title)
+
+
+    def show_history(self):
+        if self.play_history is None:
+            print('[!] Playback history is still empty')
+            return
+        
+        print('--- 📜 Play History (Most Recent) 📜 ---')
+        for i, title in enumerate(reversed(self.play_history), start=1):
+            print(f'{i}. {title}')
+            
